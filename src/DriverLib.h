c365f31b924ced3ca53ffdc29ac2ec8da0fc91a1
AngryRectangle <yashmel2000@gmail.com>
2020-12-30 22:25:07 +0400

    Move constants to DriverLib

--- src/DriverLib.h
+++ src/DriverLib.h
@@ -1,6 +1,13 @@
 #ifndef DriverLib_h
 #define DriverLib_h
 #include <Arduino.h>
+
+const int FULL_STEP = 0;
+const int HALF_STEP = 1;
+const int QUARTER_STEP = 2;
+const int EIGHTH_STEP = 3;
+const int SIXTEENTH_STEP = 4;
+
 class DriverLib {
 public:
 	DriverLib(int stepPin, int ms1Pin, int ms2Pin, int ms3Pin, int dirPin);
--- src/MultisteppingConst.h
+++
@@ -1,8 +0,0 @@
-#ifndef MultisteppingConst_h
-#define MultisteppingConst_h
-const int FULL_STEP = 0;
-const int HALF_STEP = 1;
-const int QUARTER_STEP = 2;
-const int EIGHTH_STEP = 3;
-const int SIXTEENTH_STEP = 4;
-#endif
